6b43fe4f3911ad0ee38e3b1cef7cf90e22894e81
aheit <aeita4@yahoo.com>
2026-03-08 02:39:01 +0200

    Numeric Functions

---
+++ Functions/Single Rows Finctions/NumericFunctions.sql
@@ -0,0 +1,15 @@
+/* Numeric Functions -> */
+
+
+--Round-> rounds a number to a specified number of decimal places.
+Select
+4.512,
+round(4.512,0) AS round_0,
+round(4.512,1) AS round_1,
+round(4.512,2)AS round_2
+
+--ABS -> convert negative num to positive num , remove negative sign
+
+select -10,ABS(-10)
+
+
